df007a61b33e2ed4103e651734cc2fedb04a681f
soknanprogramming <poch.soknan@gmail.com>
2025-07-06 16:09:27 +0700

    I want save my code

--- src/components/GalleryItem.tsx
+++ src/components/GalleryItem.tsx
@@ -6,14 +6,14 @@ type GalleryItemProps = {
 
 const GalleryItem: React.FC<GalleryItemProps> = ({imageURL, tittle, url}) => {
     return(
-        <a href={url}>
-            <div className='w-48 h-40 p-5 bg-blue-100 flex flex-col justify-center items-center text-center'>
+        <div>
+            <a href={url} className='w-48 h-40 p-5 bg-blue-100 flex flex-col justify-center items-center text-center'>
                 <div id="image-show" className="bg-red-400 size-20 rounded-full">
                     <img src={imageURL} className="size-20" alt="" />
                 </div>
                 <p>{tittle}</p>
-            </div>
-        </a>
+            </a>
+        </div>
     )
 }
 
